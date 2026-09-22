36a1813962226b441b15031ce279d8e8e06bca65
Lucio Duberti <luciodubb@gmail.com>
2026-09-22 16:00:47 -0300

    conecte la api

--- medio-termino/src/api/ecommerce.js
+++ medio-termino/src/api/ecommerce.js
@@ -1,12 +1,15 @@
-const base-URL = "https://ecommerce.fedegonzalez.com";
+const BASE_URL = "https://ecommerce.fedegonzalez.com";
 const TOKEN = "luciosanti";
 
-const getheaders = () => {
-
-"accept": "application/json",
-"Authorization": `Bearer ${TOKEN}`
-
-};
-
-
-
+export async function obtenerProductos() {
+  const respuesta = await fetch(`${BASE_URL}/products/?skip=0&limit=100`, {
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  if (!respuesta.ok) {
+    throw new Error(`Error HTTP: ${respuesta.status}`);
+  }
+  return respuesta.json();
+}
